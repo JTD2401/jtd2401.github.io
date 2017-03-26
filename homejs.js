830f5ff1b0d7de9498722ae0378c485192a55ebb
James DiGrazia <jtd2401@g.rit.edu>
2017-03-26 13:46:53 -0400

    Changed colors for links in topbar. Added links to "DOGS" and "AMAZE". Changed circle animations to only run once and made them stop where they are when unhovered. Added textboxes into top bar that fade in when circles are hovered. (Used jquery) Slightly edited rotatingcircles from random page.

--- homejs.js
+++ homejs.js
@@ -11,6 +11,7 @@ $(document).ready(function() {
   });
 });
 
+//Changes height of bar to circle height plus 50 every .1s
 function barHeight() {
 $( "#bar" ).each(function() {
   $( "#bar" ).height($("#c1").height() + 50);
@@ -19,3 +20,21 @@ $( "#bar" ).each(function() {
 }
 
 barHeight();
+
+//Reveal info on load
+$( "#bar" ).hover(function() {
+  $( "#barInfo" ).fadeIn( "slow" );
+});
+
+//Reveal info when circle is hovered over
+$("#c1inner").hover(function() {
+  $( "#c1info" ).fadeIn( "slow" );
+});
+
+$("#c2inner").hover(function() {
+  $( "#c2info" ).fadeIn( "slow" );
+});
+
+$("#c3inner").hover(function() {
+  $( "#c3info" ).fadeIn( "slow" );
+});
