8fe81a9ce530b5a799a5af41e4a3df619fae9793
MAIN\jtd2401 <jtd2401@g.rit.edu>
2017-04-13 16:22:14 -0400

    Starting working with JSON information

--- feedjs.js
+++ feedjs.js
@@ -8,3 +8,21 @@
     js.src=p+"://platform.twitter.com/widgets.js";
   fjs.parentNode.insertBefore(js,fjs);
 }}(document,"script","twitter-wjs");
+
+//JSON and Weatherbit.io API
+var requestURL = 'https://api.weatherbit.io/v1.0/forecast/3hourly/city?city_id=5134086&days=5&units=I&key=5b0e9dcfb5484019a66090f41621c536';
+var request = new XMLHttpRequest();
+
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+request.onload = function() {
+  var weatherInfo = request.response;
+  populateHeader(weatherInfo);
+  showWeather(weatherInfo);
+}
+
+function showWeather(jsonWeather) {
+  var cityName = jsonWeather['city_name'];
+}
